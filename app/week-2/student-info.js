bd460431d2ce52700a26364678c1f4b7d0dcb467
Leomathew25 <166417184+Leomathew25@users.noreply.github.com>
2024-10-02 16:27:43 -0600

    Added Week 2

---
+++ app/week-2/student-info.js
@@ -0,0 +1,15 @@
+import Link from "next/link";
+
+const StudentInfo = () => {
+  return (
+    <div>
+      <h1>Leo Mathew</h1>  
+      <p>My repository:</p>
+      
+         <Link href="https://github.com/Leomathew25/cprg306-assignments"> My Github </Link>
+      
+    </div>
+  );
+};
+
+export default StudentInfo;
