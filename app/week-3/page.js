4feaa302b13c7fb71d0a1e65900b14652591bf66
Leomathew25 <166417184+Leomathew25@users.noreply.github.com>
2024-10-08 21:04:52 -0600

    update code

--- app/week-3/page.js
+++ app/week-3/page.js
@@ -1,11 +1,13 @@
 
-
-const Page = () => {
-  return (
+import ItemList from "./item-list"
+export default function Page(){
+return (
     <main className="min-h-screen bg-gray-50 p-8">
       <h1 className="text-4xl font-bold text-center text-gray-800 mb-6">Shopping List</h1>
-      <ItemList />
+    <ItemList></ItemList>  
     </main>
+
   );
 };
+
 
