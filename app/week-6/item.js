b58a679eb0fdb78131827ed7bd3e1add0afd6d59
Leomathew25 <166417184+Leomathew25@users.noreply.github.com>
2024-10-22 19:27:11 -0600

    changes and week 6

---
+++ app/week-6/item.js
@@ -0,0 +1,12 @@
+export default function Item({ name, quantity, category }) {
+    return (
+      <section className="bg-zinc-700 w-60 m-2">
+        <h3 className="text-white font-bold text-xl">
+          {name},{quantity}
+        </h3>
+        <p className="text-white">
+          Buy {quantity} in {category}
+        </p>
+      </section>
+    );
+  }
---
+++ app/week-6/page.js
@@ -0,0 +1,11 @@
+import ItemList from "./item-list";
+
+export default function Page() {
+  return (
+    <main className="bg-zinc-950 p-4">
+      <h1 className="text-white font-bold text-2xl mb-5">Shopping List</h1>
+      <ItemList />
+      <h2>Week 3</h2>
+    </main>
+  );
+}
